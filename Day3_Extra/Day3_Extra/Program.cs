a4a3a109b2f93ca607cfb62f1146f219ea7e0317
zaneg789 <70683635+zaneg789@users.noreply.github.com>
2020-09-03 19:55:17 +0300

    Day3 Extra Task

---
+++ Day3_Extra/Day3_Extra/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Day3_Extra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array(int[] arr);
+
+        }
+
+        static void Array(int[] arr)
+        {
+            int[] Arr = new int[6];
+            Random ran = new Random();
+
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                int rnd = ran.Next();
+                Arr[i] = rnd;
+
+               // Console.WriteLine(Arr[i]);
+            }
+
+           int temp;
+
+            
+            for (int j = 0; j < Arr.Length - 1; j++)
+                for (int z = j + 1; z < arr.Length; z++)
+
+                     
+                    if (arr[j] < arr[z])
+                    {
+
+                        temp = arr[j];
+                        arr[j] = arr[z];
+                        arr[z] = temp;
+                    }
+
+            
+            foreach (int value in arr)
+            {
+                Console.Write(value + " ");
+            }
+
+
+
+
+        }
+        
+
+
+
+    }
+}
