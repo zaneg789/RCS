26dc086a3626df125b843da8217957d8deb7ef4b
zaneg789 <70683635+zaneg789@users.noreply.github.com>
2020-09-03 19:35:35 +0300

    projects added

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+            
+         static void Array(int [] arr)
+         {
+            int[] Arr = new int[6];
+            Random ran = new Random();
+
+            for (int i = 0; i < Arr.Length; i++)
+            {
+                int rnd = ran.Next(100);
+                Arr[i] = rnd;
+            }
+            Array.Sort(Arr);
+            foreach(int value in Arr)
+            {
+                    Console.Write(value + " "); 
+            }
+         }
+
+
+
+
+        }
+    }
+}
