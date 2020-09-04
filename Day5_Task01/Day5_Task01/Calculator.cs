b56d0c6c31afa4b23eae8031de4054eb6568efb4
zaneg789 <70683635+zaneg789@users.noreply.github.com>
2020-09-04 19:26:16 +0300

    Day5 Task01, Calculator

---
+++ Day5_Task01/Day5_Task01/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Day5_Task01
+{
+    public class Calculator
+    {
+        
+        public static double Sum (double a, double b)
+        {
+            Double result = a + b;
+            return result;
+        }
+        public static double Sub(double a, double b)
+        {
+            Double result = a - b;
+            return result;
+
+        }
+        public static double Multiplication (double a, double b)
+        {
+            Double result = a * b;
+            return result;
+        }
+        public static double Div(double a, double b)
+        {
+            Double result = a / b;
+            return result;
+        }
+
+        
+    }
+}
---
+++ Day5_Task01/Day5_Task01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Day5_Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the first digit!");
+            Double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What action do you want to take?");
+            String action = Console.ReadLine();
+            Console.WriteLine("Please enter the second digit!");
+            Double b = Convert.ToDouble(Console.ReadLine());
+
+
+            switch (action)
+            {
+                case "+":
+                    Console.WriteLine(Calculator.Sum(a, b));
+                    break;
+                case "-":
+                    Console.WriteLine(Calculator.Sub(a, b));
+                    break;
+                case "*":
+                    Console.WriteLine(Calculator.Multiplication(a, b));
+                    break;
+                case "/":
+                    Console.WriteLine(Calculator.Div(a, b));
+                    break;
+            }  
+
+
+        }
+
+
+    }
+}
