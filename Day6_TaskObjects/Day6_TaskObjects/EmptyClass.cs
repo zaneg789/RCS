8f8cc9e4d6fa4948985ceccdc760d06d4a5c622e
zaneg789 <70683635+zaneg789@users.noreply.github.com>
2020-09-07 21:24:19 +0300

    Finished Task Objects

---
+++ Day6_TaskObjects/Day6_TaskObjects/EmptyClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Day6_TaskObjects
+{
+    public class student
+    {
+        public String name { get; set; }
+
+
+        public String surname { get; set; }        
+        private int _year;
+
+        public int year
+        {
+            get
+            {
+                return _year;
+            }
+            set
+            {
+                if (value < 1)
+                {
+                    _year = 1;
+                }
+                else if (value > 3)
+                {
+                    _year = 3;
+                }
+
+                else
+                {
+                    _year = value;
+                }
+
+            }
+        }
+
+        public void PrintInfo()
+
+        {
+            Console.WriteLine(name + " " + surname + " " + year);
+        }
+
+        }
+}
---
+++ Day6_TaskObjects/Day6_TaskObjects/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Day6_TaskObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student student1 = new student();
+            Console.WriteLine("ievadiet studenta vardu!");
+            student1.name = Console.ReadLine();
+            Console.WriteLine("Ievadiet studenta uzvardu!");
+            student1.surname = Console.ReadLine();
+            Console.WriteLine("Ievadiet kursu!");
+            student1.year = Convert.ToInt32(Console.ReadLine());
+
+
+            student1.PrintInfo();
+
+
+
+        }
+    }
+
+}
