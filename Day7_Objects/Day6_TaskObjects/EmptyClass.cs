b4fa03baecc3c22102f6deda6a076d0cee7d9dc4
zaneg789 <70683635+zaneg789@users.noreply.github.com>
2020-09-08 19:52:40 +0300

    Day7_Objects

---
+++ Day7_Objects/Day6_TaskObjects/EmptyClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Day7_Objects
+{
+    public class Student
+    {
+        public String name { get; set; }
+        public String surname { get; set; }
+        public String year { get; set; }
+
+        public Student(String name, String surname, String year)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.year = year;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(name + " " + surname + " " + year);
+        }
+    }
+
+}
+
---
+++ Day7_Objects/Day6_TaskObjects/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace Day7_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student1();
+        }
+
+
+        static void Student1()
+        {
+            Student Student1 = new Student("Dace", "Bura", "2");
+            Student1.PrintInfo();
+
+        
+        }
+    }
+}
+
+        
+    
+
+
