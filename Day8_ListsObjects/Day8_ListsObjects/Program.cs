5e4dd9509f1521f4620dc8087c9912026e1eb9f7
zaneg789 <70683635+zaneg789@users.noreply.github.com>
2020-09-10 20:52:57 +0300

    Day8_Classwork

---
+++ Day8_ListsObjects/Day8_ListsObjects/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day8_ListsObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day8_Task1();
+        }
+
+        static void Day8_Task1()
+        { 
+
+        List<Taisnsturis> lstOfTaisnsturis = new List<Taisnsturis>();
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                
+                int sideA = 0;
+                int sideB = 0;
+                try
+                {
+                   Console.WriteLine("Please enter the length of the side a! ");
+                   sideA = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Please enter the length of the side b! ");
+                    sideB = Convert.ToInt32(Console.ReadLine());
+                    if (sideA < 1 || sideB < 1)
+                    {
+                        Console.WriteLine("Negativs skaitlis!");
+                        continue;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Nav pareiza ievade.");
+                    continue;
+                }
+                
+
+            
+
+
+                lstOfTaisnsturis.Add(new Taisnsturis(sideA, sideB));
+            }
+
+}
+}
+}
---
+++ Day8_ListsObjects/Day8_ListsObjects/Taisnsturis.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Day8_ListsObjects
+{
+    class Taisnsturis
+    {
+        public int SideA { get; set; }
+        public int SideB { get; set; }
+
+        public int Size()
+
+        {
+            int Size = SideA * SideB;
+            return Size;
+
+        }
+
+
+        public Taisnsturis(int sideA, int sideB)
+        {
+            SideA = sideA;
+            SideB = sideB;
+            
+        }
+
+    }
+}
