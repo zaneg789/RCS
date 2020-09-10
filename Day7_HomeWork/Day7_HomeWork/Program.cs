2db63a443c4e700227a7417b598afb7f3ccc6482
zaneg789 <70683635+zaneg789@users.noreply.github.com>
2020-09-10 10:17:14 +0300

    Homework, Day 7

---
+++ Day7_HomeWork/Day7_HomeWork/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day7_HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student();
+        }
+
+        static void Student()
+        {
+            List<Student> lstOfStudents = new List<Student>();
+
+            Console.WriteLine("What action would you like to take?");
+
+            String choice = "";
+
+            while (choice != "0")
+            {
+                Console.WriteLine("1- Add Student");
+                Console.WriteLine("2- Delete Student");
+                Console.WriteLine("3- Print Info");
+                Console.WriteLine("0- Exit");
+
+                choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Add(lstOfStudents);
+                        break;
+                    case "2":
+                        Remove(lstOfStudents);
+                        break;
+                    case "3":
+                        PrintInfo(lstOfStudents);
+                        break;
+                    case "0":
+                        break;
+                    default:
+                        Console.WriteLine("Error!");
+                        break;
+                }
+            }
+
+        }
+
+        private static void PrintInfo(List<Student> lstOfStudents)
+        {
+            for (int i = 0; i < lstOfStudents.Count; i++)
+            {
+                lstOfStudents[i].PrintInfo();
+            }
+        }
+
+        private static void Remove(List<Student> lstOfStudents)
+        {
+            Console.WriteLine("Which element would you like to delete?");
+            int toDelete = Convert.ToInt32(Console.ReadLine());
+            lstOfStudents.RemoveAt(toDelete);
+        }
+
+        private static void Add(List<Student> lstOfStudents)
+        {
+            Console.WriteLine("Enter name!");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("Enter Surname!");
+            String surname = Console.ReadLine();
+
+            Console.WriteLine("Enter year!");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            lstOfStudents.Add(new Student(name, surname, year));
+        }
+    }
+}
---
+++ Day7_HomeWork/Day7_HomeWork/Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Day7_HomeWork
+{
+    public class Student
+    {
+        
+        public String Name { get; set; }
+        public String Surname { get; set; }
+
+        public int _year;
+
+        private int year
+        {
+            get
+            {
+                return _year;
+            }
+            set
+            {
+                if (value >= 1 && value <= 3)
+                {
+                    _year = value;
+                }
+                else if (value > 3)
+                {
+                    _year = 3;
+                }
+                else
+                {
+                    _year = 1;
+                }
+            }
+
+        }
+
+        public Student(String name, String lastName, int course)
+        {
+            Name = name;
+            Surname = lastName;
+            year = course;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine(Name + " " + Surname + " " + year);
+        }
+    }
+    
+    
+}
