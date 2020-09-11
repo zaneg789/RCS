aef41426b1e0b053e3a723bfc0adba15ad5fee36
zaneg789 <70683635+zaneg789@users.noreply.github.com>
2020-09-11 20:28:19 +0300

    Day9

---
+++ Day9_Files/File.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day9_Files
+{
+    public class File
+    {
+        
+        private const String defaultName = @"~zanegrandi\projects\Day8_Files\Bin\Debug\netcoreapp3.1\";
+
+
+        public static void Read(String FileName)
+
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(defaultName + FileName);
+
+                String line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Neizdevas nolasit failu!");
+            }
+
+        }
+
+        public static void Write(List<string>lst)
+
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter("ListTest.txt", true);
+                for (int i = 0; i < lst.Count; i++)
+                {
+                    sw.WriteLine(lst[i]);
+                }
+
+                sw.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Neizdevas ierakstit faila!");
+            }
+        }
+
+
+        public static void Read(List<string> lst)
+
+        {
+            try
+            {
+                StreamReader sr = new StreamReader("ListTest.txt");
+
+                String line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+                }
+
+                sr.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Neizdevas nolasit failu!");
+            }
+
+        }
+
+
+    }
+}
---
+++ Day9_Files/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day9_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Kuru failu velaties atvert?");
+            // String FileName = Console.ReadLine();
+
+           // File.Read(FileName);
+
+            List<String> lst = new List<String>();
+
+            lst.Add("aaa");
+            lst.Add("bbb");
+            lst.Add("ccc");
+            lst.Add("ddd");
+
+            File.Write(lst);
+
+            File.Read(lst);
+        }
+    }
+}
