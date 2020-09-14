729f1d2891f6656955b4dbd7e2a92c981a5ab362
zaneg789 <70683635+zaneg789@users.noreply.github.com>
2020-09-14 21:56:19 +0300

    Day 10 Classwork

---
+++ Day10_Classwork/Day10_Classwork/FileOperations.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day10_Classwork
+{
+    class FileOperations
+    {
+        private const String defaultPath = @"~zanegrandi\projects\Day10_Classwork_Files\Bin\Debug\netcoreapp3.1\";
+        private const String filename = "Test.txt";
+
+
+        public static List<int> Read()
+        {
+            List<int> lstOfElements = new List<int>();
+            try
+            {
+
+                StreamReader sr = new StreamReader(defaultPath + filename);
+
+
+                String line = sr.ReadLine();
+
+                while (line != null)
+                {
+                    lstOfElements.Add(Convert.ToInt32(line));
+                    line = sr.ReadLine();
+                }
+                sr.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Neizdevas nolasit failu!");
+            }
+
+            return lstOfElements;
+        }
+
+        public static void Write(List<int> lst)
+        {
+                try
+                {
+                    StreamWriter sw = new StreamWriter(defaultPath + filename);
+                    sw.WriteLine(Convert.ToInt32(Console.ReadLine()));
+                }
+
+                catch
+                {
+                    Console.WriteLine("Neizdevas ierakstit faila!");
+                }
+            
+        }
+    }
+}
---
+++ Day10_Classwork/Day10_Classwork/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day10_Classwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task4();
+
+        }
+
+        static void Task4()
+        {
+            List<int> lst = new List<int>();
+
+            String choice = "";
+
+            while (choice != "0")
+            {
+                Console.WriteLine("1 - Saskaitit visu skaitlu summu.");
+                Console.WriteLine("2- Pievienot sarakstam skaitli");
+                Console.WriteLine("3- Dzest skaitli.");
+                Console.WriteLine("4- Aprekinat videjo aritmetisko.");
+                Console.WriteLine("0- Iziet.");
+
+
+                choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Sum(lst);
+                        break;
+                    case "2":
+                        AddElement(lst);
+                        break;
+                    case "3":
+                        RemoveElement(lst);
+                        break;
+                    case "4":
+                        Average(lst, Sum(lst));
+                        break;
+                    case "0":
+                        break;
+                    default:
+                        Console.WriteLine("Nepareiza ievade");
+                        break;
+                }
+            }
+        }
+
+
+
+        private static double Average(List<int> lstOfElements, int sum)
+        {
+            double result = sum / lstOfElements.Count;
+            return result;
+        }
+
+
+
+        private static int Sum(List<int> lstOfElements)
+        {
+            int sum = 0;
+            for (int i = 0; i < lstOfElements.Count; i++)
+            {
+                sum = sum + lstOfElements[i];
+
+            }
+            return sum;
+        }
+
+        private static void RemoveElement(List<int> lst)
+        {
+            Console.WriteLine();
+
+            if (lst.Count == 0)
+            {
+                Console.WriteLine("Saraksts ir tukss!");
+                return;
+            }
+
+            try
+            {
+                Console.WriteLine("Kuru elementu velaties dzest?");
+                int toDelete = Convert.ToInt32(Console.ReadLine());
+                lst.RemoveAt(toDelete);
+                FileOperations.Write(lst);
+            }
+            catch
+            {
+                Console.WriteLine("Kludaina ievade!");
+            }
+
+
+            Console.WriteLine();
+        }
+
+        private static void AddElement(List<int> lst)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Ievadiet elementu!");
+            lst.Add(Convert.ToInt32(Console.ReadLine()));
+            FileOperations.Write(lst);
+
+        }
+    }
+}
+
